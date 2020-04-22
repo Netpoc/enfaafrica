e90f642d10144ad66fe93f2e7f6dba2188f8cf15
Netpoc AgroTech <ayenotopeter@hotmail.com>
2020-04-22 15:04:17 +0100

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,148 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
+    <v-navigation-drawer
+      width="200"
+      v-model="drawer"
+      color="#DEC227"
+      absolute
       dark
+      temporary
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+      
+      <v-list dense>
+        <v-list-item-group >
+          <v-list-item-icon>
+            <v-icon> </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content class="ma-1">
+            <v-btn text to="/">Home </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn text to="/Team">The Team </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn text to="/Services">Our Services </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn text to="/Contact">Contact Us </v-btn>
+          </v-list-item-content>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    
+    
     <v-content>
-      <HelloWorld/>
+      
+      
+        
+    <router-view/>
+      
     </v-content>
+    <v-btn
+      bottom
+      color="green"
+      dark
+      fixed
+      large
+      right
+      fab
+      href="https://api.whatsapp.com/send?phone=2348118300383"
+      target="_blank"
+    >
+      <v-icon>mdi-whatsapp</v-icon>
+    </v-btn>
+
+    <v-footer
+      app
+      absolute
+      padless
+      inset
+      tile
+      >
+<v-card width="100%" color="#4D4D4D" dark>
+  <v-row align="center">
+    <v-col>
+        <v-btn
+        icon
+        >
+          <v-icon size="24px">mdi-twitter </v-icon>
+        </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn
+        icon
+        >
+          <v-icon size="24px">mdi-facebook </v-icon>
+        </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn
+        icon
+        >
+          <v-icon size="24px">mdi-linkedin </v-icon>
+        </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn
+        icon
+        >
+          <v-icon size="24px">mdi-instagram </v-icon>
+        </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn
+        icon
+        href="https://api.whatsapp.com/send?phone=2348118300383"
+        >
+          <v-icon size="24px">mdi-whatsapp</v-icon>
+        </v-btn>
+
+      <br>
+
+
+        &copy; {{ new Date().getFullYear() }} | <strong>Netpoc Inc.</strong>
+    </v-col>
+  </v-row>
+</v-card>
+
+    
+  
+    </v-footer>
+    
   </v-app>
+ 
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+  data() {
+    return {
+      drawer: null,
 
-  components: {
-    HelloWorld,
+    }
   },
-
-  data: () => ({
-    //
-  }),
-};
+  watch: {
+        '$route' (to) {
+          document.title = to.meta.title || 'Netpoc Inc.'
+        }
+      },
+}
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif, 'Courier New', Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 0px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container fluid>
+    <v-row align="start" justify="space-between">
+      <v-col md="3">
+        <v-img src="@/assets/ENFA.png" max-width="350"></v-img>
+      </v-col>
+      <v-col md="8">
+        
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
